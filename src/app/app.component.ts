2a49ccbd74e3f0086f29698f3a2e369908810423
Aram <aramsuqiasyan20@gmail.com>
2024-08-26 17:58:57 +0400

    feat: create table and audio use mat-table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {CommonModule} from "@angular/common";
+import {
+  MatCell,
+  MatTable,
+  MatCellDef,
+  MatHeaderRow,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderRowDef,
+  MatHeaderCellDef,
+  MatRow, MatRowDef,
+} from "@angular/material/table";
+
+interface AudioRecord {
+  id: number;
+  name: string;
+  fileName: string;
+}
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    MatRow,
+    MatCell,
+    MatTable,
+    MatCellDef,
+    MatRowDef,
+    RouterOutlet,
+    MatColumnDef,
+    MatHeaderRow,
+    MatHeaderCell,
+    MatHeaderRowDef,
+    MatHeaderCellDef,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'test-table';
+  displayedColumns: string[] = ['id', 'name', 'fileName'];
+  dataSource: AudioRecord[] = [
+    {id: 1, name: 'Sound 1', fileName: 'sound1.wav'},
+    {id: 2, name: 'Sound 2', fileName: 'sound2.wav'},
+    {id: 3, name: 'Sound 3', fileName: 'sound3.wav'}
+  ];
+  selectedAudio: AudioRecord | null = null;
+
+  selectAudio(row: AudioRecord) {
+    this.selectedAudio = row;
+  }
+
 }
